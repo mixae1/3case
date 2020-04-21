e7d80a9a622d0e893157f807e9289d7c7aff37f7
Anoth <mixaetel@gmail.com>
2020-04-21 23:46:59 +0300

    добавил токен

---
+++ VKParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VkNet;
+using VkNet.Model;
+using VkNet;
+namespace SocNetParser
+{
+    class VKParser
+    {
+
+    }
+}
