5e383479401fcf9c89c45418b2ee8251da8db483
selfmadekirich <37487158+selfmadekirich@users.noreply.github.com>
2020-05-05 20:02:54 +0300

    добавил метод  дополнения инфы с карт

--- Companies.cs
+++ Companies.cs
@@ -5,66 +5,57 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using System.Threading;
+using System.Text.Json;
+using VkNet.Exception;
+using System.Net.Http;
+
 namespace SocNetParser
 {
     class Companies
     {
 
-       
         public static string test = "https://rostov-na-donu.bizly.ru/remont-akpp/page-2/";
 
 
         static Regex RND_reg = new Regex(@"<div class=\Dtitle\D>([\s\S]+?)<\/ul>");
         static Regex RND_name = new Regex(@"<a href([\s\S])+?>([\s\S]+?)[<\/a>]");
 
-        // @"<div class=\Dorg\D>([\s\S]+?)<div class=\Dto-top\D>" (2-5) --- РЕГУЛЯРКА К СТАРОМУ САЙТУ ПОКА ПРОШУ НЕ ТРОГАТЬ
 
-
-        public static Regex websites = new Regex(@"\D?http[s]?:\/\/([а-яА-Яa-zA-Z0-9]\.?[-]?)+\.\w{1,4}\D?");
+       static WebClient webclient = new WebClient();
+      
 
         /// <summary>
         /// получаем список компаний определенного бизнес сектора 
         /// TODO : сделать для любого бизнеса
         /// </summary>
         /// <returns></returns>
-        public static void FullFillCompanies(BusinessPage bs, Predicate<string> pred, int Compsneeds = 1)
+        public static void GetCompanies(BusinessPage bs, Predicate<string> pred, int Compsneeds = 1)
         {//13 мс
 
             string page = "page-";
-             int index = 0;
+            // int index = 1;
             var lst = new List<Company>();
-            WebClient webclient = new WebClient();
-            webclient.Headers.Add(HttpRequestHeader.UserAgent, "");
 
-
-
-            for (int i = 1; i < 50; i++)//до 5 потому что пока парсим сайт кафе ,там 58 страниц ,точно ошибки не будет 
+            // var comps = RND_reg.Matches(File.ReadAllText("buf1.txt"));
+            for (int i = 1; i < 3; i++)
             {
                 string sourse = bs.webName + "/" + page + i;
-                var buf = webclient.DownloadString(sourse);
-                var comps = RND_reg.Matches(buf);
+                WebRequest wr = WebRequest.Create(sourse);
+                WebResponse wren = wr.GetResponse();
+                var temp = new StreamReader(wren.GetResponseStream());
+                var comps = RND_reg.Matches(temp.ReadToEnd());
 
 
                 foreach (Match x in comps)//16мс в 1 раз 
                 {
-
-                    string webs;
-                    var web = websites.Matches(x.Value);
-                    if (web.Count == 1) webs = null;
-                    else
-                        webs = web.Last().Value.Trim();
-
-                   var temp = GetVkFaceInst(webs);
-
-                                          
-
                     HashSet<string> ad = new HashSet<string>();
+
                     var tmp = SiteParser.adress.Matches(x.ToString());//1-2
                     ad = new HashSet<string>();
                     foreach (var xx in tmp)
                         ad.Add(xx.ToString().Trim());
 
-                    if (ad.Count!=0 && !pred(ad.First())) continue;
+                    if (!pred(ad.First())) continue;
 
                     var address = ad;
                     ad = new HashSet<string>();
@@ -76,6 +67,11 @@ namespace SocNetParser
 
 
 
+                    string webs;
+                    var web = SiteParser.websites.Matches(x.Value);
+                    if (web.Count == 1) webs = null;
+                    else
+                        webs = web.Last().Value.Trim();
 
                     List<string> phones = new List<string>();
                     tmp = SiteParser.phone.Matches(x.Value);
@@ -87,52 +83,105 @@ namespace SocNetParser
 
                     string name = RND_name.Match(x.Value).Groups[2].Value;//2-3мс
 
-                    lst.Add(new Company(name, address, ad, webs, phones,temp.Item1,temp.Item2,temp.Item3));//3-4 мс
+                    lst.Add(new Company(name, address, ad, webs, phones));//3-4 мс
                 }
 
-               // Thread.Sleep(500);// чтоб нами не заинтересовались админы сайта 
+                Thread.Sleep(1000);// чтоб нами не заинтересовались админы сайта 
             }
 
             bs.comps = lst;
         }
 
+        /// <summary>
+        /// получаем список компаний с json файла
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static List<Company> GetCompaniesJson(string path) 
+        { 
+            return JsonSerializer.Deserialize<List<Company>>(File.ReadAllText(path));
+        }
 
 
-        static (string, string, string) GetVkFaceInst(string website)
+        public static void AddCompanyInfo(List<Company> lst)
         {
-            if (website == null) return (null, null, null);
-            WebClient wb = new WebClient();
+            int counter = 0;
+            foreach (var tmp in lst)
+            {    counter++;
+                if (string.IsNullOrEmpty(tmp.website)) continue;
+                AddInfo(tmp);
+                Console.WriteLine($"{counter} company from 25 was fullfilled");
+               
+            }
+        }
+
+        static void AddInfo(Company comp)
+        {
             
-            var temp = (string.Empty, string.Empty, string.Empty);
+            string ur = "http://";
+
+            string s;
+
             try
             {
-                string tmp = wb.DownloadString(website);
-                temp = (SiteParser.vk.Match(tmp).Value, SiteParser.face.Match(tmp).Value, SiteParser.inst.Match(tmp).Value);
+                s = webclient.DownloadString(ur + comp.website);
             }
-            catch (Exception e)
+            catch
             {
-               // Console.WriteLine("no website ot website is incorrect");
+                return;
             }
-            return temp;
+
+         //   if (s == null) return;
+
+            var phns = SiteParser.phone.Matches(s);
+            if (comp.phones == null) comp.phones = new List<string>(); 
+            foreach(var x in phns)
+            {
+                comp.phones.Add(x.ToString());
+            }
+
+            var ems = SiteParser.mail.Matches(s);
+            if (comp.emails == null) comp.emails = new HashSet<string>();
+            foreach (var x in ems)
+            {
+                comp.emails.Add(x.ToString());
+            }
+
+            var vk = SiteParser.vk.Match(s);
+            if (vk.Success)
+            {
+                if ((comp.vk == null) || (comp.vk != null)
+                && (!comp.vk.Contains(vk.Value) || !vk.Value.Contains(comp.vk)))
+                    comp.vk = vk.Value;
+            }
+
+            var face = SiteParser.face.Match(s);
+            if (face.Success)
+            {
+                if ((comp.face == null) || (comp.face != null)
+                    && (!comp.face.Contains(vk.Value) || !face.Value.Contains(comp.face)))
+                    comp.face = face.Value;
+            }
+
+            var inst = SiteParser.inst.Match(s);
+            if (inst.Success)
+            {
+                if ((comp.inst == null) || (comp.inst != null) 
+                    && (!comp.inst.Contains(inst.Value) || !inst.Value.Contains(comp.inst)))
+                    comp.inst = inst.Value;
+            }
+
+            var adrs = SiteParser.adress.Matches(s);
+            if (comp.adress == null) comp.adress = new HashSet<string>();
+            foreach (var x in adrs)
+            {
+                comp.adress.Add(x.ToString());
+            }
+
+
         }
 
-        // пусть пока кол-во возвращает ; здесь можно класс прикрутить
-        /*
-        static int? GetPost(string url)
-        {
-            if (url == null) return null;
 
-            VKParser vk=new VKParser
-        }
-
-    */
-        public static bool AppAdress(string adress, string[] streets)
-        {
-            foreach (var x in streets)
-                if (adress.Contains(x)) return true;
-            return false;
-
-        }
 
 
 
@@ -146,19 +195,26 @@ namespace SocNetParser
             {
                 Console.WriteLine(temp.name);
 
-                foreach (var tmp in temp.adress)
-                    Console.WriteLine(tmp);
+                if (temp.adress == null) Console.WriteLine("no adress");
+                else 
 
-                foreach (var tmp in temp.emails)
-                    Console.WriteLine(tmp);
+                 foreach (var tmp in temp.adress)
+                  Console.WriteLine(tmp);
 
-                if (temp.phones.Count > 0)
-                    Console.WriteLine(temp.phones[0]);
+
+                if (temp.emails == null) Console.WriteLine("no phones"); 
+                else 
+               foreach (var tmp in temp.emails)
+                   Console.WriteLine(tmp);
+
+               if (temp.phones.Count > 0)
+                 Console.WriteLine(temp.phones[0]);
 
                 Console.WriteLine(temp.website);
-                Console.WriteLine($"face ={temp.face}");
-                Console.WriteLine($"inst ={temp.inst}");
-                Console.WriteLine($"vk ={temp.vk}");
+                Console.WriteLine(temp.face);
+                Console.WriteLine(temp.vk);
+                Console.WriteLine(temp.inst);
+
 
                 Console.WriteLine("//////////");
             }
