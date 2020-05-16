318600be0c376324d0fedaee9c61c122087eb788
selfmadekirich <37487158+selfmadekirich@users.noreply.github.com>
2020-05-16 23:55:05 +0300

    пофиксил баг

--- Linqer.cs
+++ Linqer.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using System.Threading;
+using SocNetParser.Properties;
+using System.Text.RegularExpressions;
 
 namespace SocNetParser
 {
@@ -15,26 +19,31 @@ namespace SocNetParser
             
         }
 
-
+        /// <summary>
+        /// начинает процесс сбоки информации : обходит вк 
+        /// TODO: обойти сайт с технической инфой и инсту прикрутить
+        /// </summary>
         public  void StartLinq()
         {
             foreach (var tmp in pages)
             {
                 var vkurls = tmp.GetVkUrls();
-              
+                var domains = tmp.GetDomains();
+
                Task<List<DateTime?>> vktask =Task.Run(()=> GetVkPostDateTime(vkurls));
+                Task<List<(DateTime? regdate, DateTime? expdate, DateTime? upddate)>> techtask = Task.Run(() => GetTechSiteInfo(domains));
                 /*
                  * 
                  * + еще парсеры: 
-                 * 1)сайта для получения инфы о дате регистрации домена и дате окончания лицензии 
                  * 2) инсты
                  * 3) еще какой-нибудь херни 
                  * !!!!!! если будут траблы с производительностью на компе или серваке сразу написать мне!!!!!
                  * 
                 */
-              Task.WaitAll(vktask);
+              Task.WaitAll(vktask,techtask);
                 var vkres = vktask.Result;
-                AddInfoInPage(vkres);
+                var techres = techtask.Result;
+                AddInfoInPage(vkres,techres);
             }
         }
 
@@ -48,11 +57,11 @@ namespace SocNetParser
         {
             Console.WriteLine("getting vk info:");
             List<DateTime?> vkUrls = new List<DateTime?>();
-            int counter = 0;
+           // int counter = 0;
             foreach (var tmp in urls)
             {
                 DateTime? dt = null;
-                counter++;
+               // counter++;
                 if (!string.IsNullOrEmpty(tmp))
                 {
                     var vk = new VKGroup(tmp);
@@ -60,7 +69,7 @@ namespace SocNetParser
                         dt = vk.DTofLastTimePostedWithWall();
                 }
                 vkUrls.Add(dt);
-                Console.WriteLine($"{counter} from 25");
+                //Console.WriteLine($"{counter} from 25");
                
             }
 
@@ -68,31 +77,104 @@ namespace SocNetParser
         }
 
         /// <summary>
+        /// основной метод получения инфы с whois 
+        /// </summary>
+        /// <param name="urls"></param>
+        /// <returns></returns>
+        List<(DateTime? regdate, DateTime? expdate, DateTime? upddate)> GetTechSiteInfo(List<string> urls)
+        {
+            List<(DateTime?, DateTime?, DateTime?)> lst = new List<(DateTime?, DateTime?, DateTime?)>();
+
+            Console.WriteLine("starting parsing Whois.ru");
+            WebClient wb = new WebClient();
+            int counter = 0; // по условию юзанья там стоит ограничение в 30 запросов за минуту 
+            DateTime counterTime = DateTime.Now;
+            foreach (var tmp in urls)
+            {
+                double secondsPerMinPassed = DateTime.Now.Subtract(counterTime).TotalSeconds;
+                if (secondsPerMinPassed < 60 && counter == 30)
+                {
+                    Thread.Sleep(60-(int)secondsPerMinPassed); // контролируем ограничение в 30 запрсов/мин 
+                    counter = 0;                            // если уже прошла минута и было сделано 30 то засыпаем на остаток минуты
+                    counterTime = DateTime.Now;
+                }
+
+
+                if (string.IsNullOrEmpty(tmp))
+                {
+                    lst.Add((null, null, null));
+                    continue;
+                }
+
+                string ans = null;
+                try
+                {
+                    ans = wb.DownloadString(Resources.TechSiteApi+tmp); 
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message+" error was at "+tmp);
+                }
+
+                if (ans == null) { lst.Add((null, null, null)); continue; }
+                var temp = GetDates(ans,tmp);
+                counter++;
+
+                DateTime? nll = null;
+                DateTime? crDate = string.IsNullOrEmpty(temp.creationDate) ? nll: DateTime.Parse(temp.creationDate.Trim());
+                DateTime? update = string.IsNullOrEmpty(temp.updDate)?  nll: DateTime.Parse(temp.updDate.Trim()); 
+                DateTime ? expDate= string.IsNullOrEmpty(temp.expDate) ? nll: DateTime.Parse(temp.expDate.Trim());
+                lst.Add((crDate, update, expDate));
+
+               
+            }
+
+            return lst;
+        }
+
+        //вспомогательный метод
+        private (string creationDate, string updDate, string expDate) GetDates(string ans,string domain)
+        {
+            if (SiteParser.CheckAvail.IsMatch(ans))
+            {
+                if (Regex.IsMatch(domain, @"\.ru"))
+                    return (
+                          SiteParser.RusDomainCr.Match(ans).Groups[1].Value, null,
+                          SiteParser.RusDomianTill.Match(ans).Groups[1].Value);
+                return (
+                     SiteParser.EngDomainCr.Match(ans).Groups[1].Value,
+                     SiteParser.EngDomainUpd.Match(ans).Groups[1].Value,
+                     SiteParser.EngDomainExpr.Match(ans).Groups[1].Value);     
+               
+            }
+            return (null, null, null);
+        }
+
+
+
+
+        /// <summary>
         /// добавляет полученную инфу в компании определенного бизнес-типа
         /// </summary>
         /// <param name="vkdates"></param>
         /// <param name="techdate"></param>
-        void AddInfoInPage(List<DateTime?> vkdates,List<(DateTime,DateTime)> techdate=null)
+        void AddInfoInPage(List<DateTime?> vkdates,List<(DateTime?,DateTime?, DateTime?)> techdate=null)
         {
             Console.WriteLine("adding all info:");
-            Console.WriteLine(vkdates.Count+" размер списка ");
+            Console.WriteLine(techdate.Count+" размер списка " );
             foreach (var tmp in pages.Select(x => x.comps))
             {
-                int vkind = 0;
-                int techind = 0;
+               
                 int counter = 0;
                 foreach (var temp in tmp)
                 {
-                    counter++;
-                    temp.lastVkPost = vkdates[vkind];
-                    vkind++;
-                    if (techdate == null) continue;
-                    else
-                    { //убрать блок if после добавления парсера сайта 
-                        temp.registraionDomain = techdate[techind].Item1;
-                        temp.registraionDomain = techdate[techind].Item2;
+                    
+                    temp.lastVkPost = vkdates[counter];
+                    
+                    temp.registraionDomain = techdate[counter].Item1;
+                    temp.UptDomain = techdate[counter].Item2;
+                    temp.expireDomain = techdate[counter].Item3;
 
-                    }
+                    counter++;
                     Console.WriteLine($"{counter} from 25");
                 }
                     
